626741a5ef5b19e6045fe6f5760a834ff400822f
Nyambi Blaise <34681563+nyambiblaise@users.noreply.github.com>
2026-01-13 15:23:29 -0500

    Update chat.php

--- chat.php
+++ chat.php
@@ -49,7 +49,6 @@ function decryptTextV2(string $cipherText, string $salt) {
         'SecureTextTool:v2'
     );
 }
-
 /* ================= API MODE ================= */
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ajax'])) {
     header('Content-Type: application/json');
@@ -211,6 +210,7 @@ window.onload=()=>{
 </div>
 </body>
 </html>
+
 
 
 
